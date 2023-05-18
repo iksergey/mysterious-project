9a96caf87dc3d5d8ae910ef8efd9ff2f1a4d5901
Sergei Kamianetskii <i@ksergey.ru>
2023-05-19 01:58:41 +0300

    Компонента вычисления площадей

---
+++ Mathematics/CalculateArea/CalculateArea.cs
@@ -0,0 +1,23 @@
+﻿using Mathematics.Shapes;
+
+namespace Mathematics.CalculateArea
+{
+  public class CalculateArea : ICalculate
+  {
+    private void Calculate(Shape shape, ref double area)
+    {
+      area += shape.Area;
+      foreach (var item in shape.Shapes)
+      {
+        Calculate(item, ref area);
+      }
+    }
+
+    public double Visit(Shape shape)
+    {
+      double area = 0;
+      Calculate(shape, ref area);
+      return area;
+    }
+  }
+}
---
+++ Mathematics/CalculateArea/ICalculate.cs
@@ -0,0 +1,9 @@
+﻿using Mathematics.Shapes;
+
+namespace Mathematics.CalculateArea
+{
+  public interface ICalculate
+  {
+    double Visit(Shape shape);
+  }
+}
